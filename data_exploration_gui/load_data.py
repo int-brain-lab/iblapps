bd1143d7a84dba27c27590ff47899d4e33802bc4
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2020-02-19 17:34:06 +0000

    Change load_data so you can parse subject, date and session no.

--- data_exploration_gui/load_data.py
+++ data_exploration_gui/load_data.py
@@ -1,12 +1,23 @@
-
+import argparse
 from oneibl.one import ONE
 
-one = ONE()
-session_number = 1
-eid = one.search(subject='SWC_014', date='2019-12-10', number=session_number)[0]
-data_path = one.load(eid, clobber=False, download_only=True)
 
-# Uncomment to also load ephys.bin files for waveform plots
-#ephys_types = ['ephysData.raw.ch', 'ephysData.raw.meta', 'ephysData.raw.ap']
-#ephys_types = ['sync.channels', 'sync.polarities', 'sync.times']
-#ephys_path = one.load(eid, dataset_types=ephys_types, clobber=False, download_only=True)
+parser = argparse.ArgumentParser(description='Load in subject info')
+
+parser.add_argument('-s', '--subject', default=False, required=True, help='Subject Name')
+parser.add_argument('-d', '--date', default=False, required=True, help='Date of session YYYY-MM-DD')
+parser.add_argument('-n', '--session_no', default=False, required=True, help='Session Number')
+parser.add_argument('-e', '--ephys_data', default=False, required=False, help='Set True to load ephys.bin data')
+args = parser.parse_args()
+
+one = ONE()
+eid = one.search(subject=str(args.subject), date=str(args.date), number=args.session_no)[0]
+data_path = one.load(eid, clobber=False, download_only=True)
+print(f'Successfully loaded data for {args.subject} {args.date} session no. {args.session_no}')
+
+if args.ephys_data:
+    print('Loading ephys.bin data')
+    ephys_types = ['ephysData.raw.ch', 'ephysData.raw.meta', 'ephysData.raw.ap']
+    ephys_path = one.load(eid, dataset_types=ephys_types, clobber=False, download_only=True)
+else:
+    print('Ephys flag set to false... will not load ephys data')
