6f04430290dbe894d26bceab4e4d4c80f1f1158c
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2020-04-03 13:07:58 +0100

    added functionality to return infiniteLine position when enter key hit

---
+++ atlaselectrophysiology/CustomEventWidget.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtGui
+
+
+class CustomEventWidget(QtGui.QWidget):
+
+    keyPressed = QtCore.pyqtSignal(int)
+    
+    def __init__(self):
+        super(CustomEventWidget, self).__init__()
+
+    def keyPressEvent(self, event):
+        self.keyPressed.emit(event.key())
--- atlaselectrophysiology/initial_gui.py
+++ atlaselectrophysiology/initial_gui.py
@@ -3,6 +3,8 @@ import pyqtgraph as pg
 import pyqtgraph.exporters
 from pathlib import Path
 
+from CustomEventWidget import CustomEventWidget
+
 import numpy as np
 import load_data as ld
 
@@ -12,17 +14,30 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
 
         self.resize(1000, 800)
-        main_widget = QtWidgets.QWidget()
+        self.menuBar
+        main_widget = CustomEventWidget()
         self.setCentralWidget(main_widget)
         main_widget_layout = QtWidgets.QHBoxLayout()
-
+        
+        #Load the data
         eid, one = ld.get_session('ZM_2407', '2019-11-07')
         scatter_data, probe_label = ld.get_scatter_data(eid, one, probe_id=0)
         histology_data = ld.get_histology_data(eid, probe_label, one)
 
+      
+        #Menu options --> Still need to work on this
+        menuBar = QtWidgets.QMenuBar(self)
+        options = menuBar.addMenu("Plot Options")
+        menuBar.setGeometry(QtCore.QRect(0, 0, 1002, 22))
+        options.addAction('Scatter Plot')
+        options.addAction('Depth Plot')
+        self.setMenuBar(menuBar)
 
-        #Create scatter plot (for now only plot first 60s)
-        #--> to do: functionality to scroll through session
+        #Holds the infiniteLine objects added to plot
+        self.added_lines = []
+
+
+        #Create scatter plot
         print(len(scatter_data['times']))
         self.fig_scatter = pg.PlotWidget(background='w')
         self.fig_scatter.scene().sigMouseClicked.connect(self.on_mouse_double_clicked)
@@ -34,8 +49,6 @@ class MainWindow(QtWidgets.QMainWindow):
         #self.scatter_plot = pg.ScatterPlotItem()
         #self.scatter_plot.setData(x=scatter_data['times'][:], y=scatter_data['depths'][:], size = 2, color = 'k')
         self.fig_scatter.addItem(self.scatter_plot)
-
-
 
         #Create histology figure
         #--> to do: incorporate actual data into plot
@@ -54,6 +67,9 @@ class MainWindow(QtWidgets.QMainWindow):
             colour = QtGui.QColor(histology_data['boundary_colour'][idx])
             curve_item.setBrush(colour)
             self.fig_histology.addItem(curve_item)
+
+        #When you hit enter key and three Infinitelines added -> print y position of lines
+        main_widget.keyPressed.connect(self.on_enter_clicked)
 
         main_widget_layout.addWidget(self.fig_scatter)
         main_widget_layout.addWidget(self.fig_histology)
@@ -78,8 +94,12 @@ class MainWindow(QtWidgets.QMainWindow):
             line_hist = pg.InfiniteLine(pos=pos.y(), angle=0, pen='k', movable=True)
             self.fig_histology.addItem(line_scat)
             self.fig_scatter.addItem(line_hist)
-            print(pos)       
-            print(pos_atlas)
+
+            lines = [line_scat, line_hist]
+            self.added_lines.append(lines)
+      
+            #print(pos)       
+            #print(pos_atlas)
             #todo: map atlas y position to same coordinate system as scatter y position
             #then take click positions into 3 y values on each, and fit line to 3 points. 
             # [histology_data['boundary_label']]
@@ -87,6 +107,14 @@ class MainWindow(QtWidgets.QMainWindow):
             #a = pos.y() b=pos_atlas.y()
             #z = np.polyfit(a,b,1)
 
+    def on_enter_clicked(self):
+        if len(self.added_lines) == 3:
+            scatter_line_pos = [line[0].pos().y() for line in self.added_lines]
+            print(scatter_line_pos)
+            hist_line_pos = [line[1].pos().y() for line in self.added_lines]
+            print(hist_line_pos)
+        else:
+            print('need to add 3 lines')
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
