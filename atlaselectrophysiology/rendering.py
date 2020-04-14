df9ed509fccca14e9e5473f20beca0d7149ba4f0
olivier <olivier.winter@hotmail.fr>
2020-04-14 18:13:37 +0100

    add mesh function - cv2 frames rgb 2 bgr

--- atlaselectrophysiology/rendering.py
+++ atlaselectrophysiology/rendering.py
@@ -7,7 +7,6 @@ import vtk
 import mayavi.mlab as mlab
 
 
-
 def color_cycle(ind=None):
     """
     Gets the matplotlib color-cycle as RGB numpy array of floats between 0 and 1
@@ -32,15 +31,15 @@ def color_cycle(ind=None):
         return tuple(c[ind % c.shape[0], :])
 
 
-def figure(grid=False):
+def add_mesh(fig, obj_file, color=(1., 1., 1.), opacity=0.4):
     """
-    Creates a mayavi figure with the brain atlas mesh
-    :return: mayavi figure
+    Adds a mesh object from an *.obj file to the mayavi figure
+    :param fig: mayavi figure
+    :param obj_file: full path to a local *.obj file
+    :param color: rgb tuple of floats between 0 and 1
+    :param opacity: float between 0 and 1
+    :return: vtk actor
     """
-    fig = mlab.figure(bgcolor=(1, 1, 1))
-
-    # engine = mlab.get_engine() # Returns the running mayavi engine.
-    obj_file = Path(__file__).parent.joinpath("root.obj")
     reader = vtk.vtkOBJReader()
     reader.SetFileName(str(obj_file))
     reader.Update()
@@ -48,8 +47,22 @@ def figure(grid=False):
     mapper.SetInputConnection(reader.GetOutputPort())
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
-    actor.GetProperty().SetOpacity(0.6)
+    actor.GetProperty().SetOpacity(opacity)
+    actor.GetProperty().SetColor(color)
     fig.scene.add_actor(actor)
+    fig.scene.render()
+    return mapper, actor
+
+
+def figure(grid=False):
+    """
+    Creates a mayavi figure with the brain atlas mesh
+    :return: mayavi figure
+    """
+    fig = mlab.figure(bgcolor=(1, 1, 1))
+    # engine = mlab.get_engine() # Returns the running mayavi engine.
+    obj_file = Path(__file__).parent.joinpath("root.obj")
+    mapper, actor = add_mesh(fig, obj_file)
 
     if grid:
         # https://vtk.org/Wiki/VTK/Examples/Python/Visualization/CubeAxesActor
@@ -79,6 +92,8 @@ def figure(grid=False):
 
 def rotating_video(output_file, mfig, fps=12, secs=6):
     # ffmpeg -i input.webm -pix_fmt rgb24 output.gif
+    # ffmpeg -i certification.webm -vf scale=640:-1 -r 10 -f image2pipe -vcodec ppm - | convert -delay 5 -loop 0 - certification.gif  # noqa
+
     file_video = Path(output_file)
     if file_video.suffix == '.avi':
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -91,7 +106,7 @@ def rotating_video(output_file, mfig, fps=12, secs=6):
     mfig.scene.render()
     mfig.scene._lift()
     frame = mlab.screenshot(figure=mfig, mode='rgb', antialiased=True)
-    w, h, _= frame.shape
+    w, h, _ = frame.shape
     video = cv2.VideoWriter(str(file_video), fourcc, float(fps), (h, w))
 
     # import time
@@ -101,7 +116,7 @@ def rotating_video(output_file, mfig, fps=12, secs=6):
         mfig.scene.render()
         frame = mlab.screenshot(figure=mfig, mode='rgb', antialiased=True)
         print(e, (h, w), frame.shape)
-        video.write(frame)
+        video.write(np.flip(frame, axis=2))  # bgr instead of rgb...
         # time.sleep(0.05)
 
     video.release()
