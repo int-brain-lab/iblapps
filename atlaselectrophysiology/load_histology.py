e3c9209a962a302c45940a340e2d674306412a72
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2020-05-19 17:45:35 +0100

    function to load histology image from flatiron

--- atlaselectrophysiology/load_histology.py
+++ atlaselectrophysiology/load_histology.py
@@ -1,44 +1,73 @@
 
-from pathlib import Path, PurePath
+from pathlib import Path
 import requests
 import re
 from ibllib.io import params
 from oneibl.webclient import http_download_file
-from oneibl.one import ONE
 import SimpleITK as sitk
 
-def download_histology_data(subject, lab, path):
+def download_histology_data(subject, lab):
 
     par = params.read('one_params')
     FLAT_IRON_HIST_REL_PATH = Path('histology', lab, subject, 'downsampledStacks_25', 'sample2ARA')
     baseurl = (par.HTTP_DATA_SERVER + '/' + '/'.join(FLAT_IRON_HIST_REL_PATH.parts))
     try:
+        FLAT_IRON_HIST_REL_PATH = Path('histology', lab, subject, 'downsampledStacks_25', 'sample2ARA')
+        baseurl = (par.HTTP_DATA_SERVER + '/' + '/'.join(FLAT_IRON_HIST_REL_PATH.parts))
         r = requests.get(baseurl, auth=(par.HTTP_DATA_SERVER_LOGIN, par.HTTP_DATA_SERVER_PWD))
         r.raise_for_status()
     except Exception as err:
         print(err)
-        path_to_nrrd = None
-        #return path_to_nrrd
-    else:
-        tif_files = []
-        for line in r.text.splitlines():
-            result = re.findall('href="(.*)GR.tif"', line)
-            if result:
-                tif_files.append(result[0] + 'GR.tif')
+        try:
+            subject_rem = subject.replace("_", "")
+            FLAT_IRON_HIST_REL_PATH = Path('histology', lab, subject_rem, 'downsampledStacks_25', 'sample2ARA')
+            baseurl = (par.HTTP_DATA_SERVER + '/' + '/'.join(FLAT_IRON_HIST_REL_PATH.parts))
+            r = requests.get(baseurl, auth=(par.HTTP_DATA_SERVER_LOGIN, par.HTTP_DATA_SERVER_PWD))
+            r.raise_for_status()
+        except Exception as err:
+            print(err)
+            path_to_nrrd = None
+            return path_to_nrrd
 
-        CACHE_DIR = Path(Path.home(), 'Downloads', 'FlatIron', lab, 'Subjects', subject, 'histology')
-        CACHE_DIR.mkdir(exist_ok=True, parents=True)
-        path_to_image = Path(CACHE_DIR, tif_files[0])
-        if not path_to_image.exists():
-            url = (baseurl + '/' + tif_files[0])
-            http_download_file(url, cache_dir=CACHE_DIR,
-                               username=par.HTTP_DATA_SERVER_LOGIN,
-                               password=par.HTTP_DATA_SERVER_PWD)
+    tif_files = []
+    for line in r.text.splitlines():
+        result = re.findall('href="(.*)GR.tif"', line)
+        if result:
+            tif_files.append(result[0] + 'GR.tif')
 
-        path_to_nrrd = tif2nrrd(path_to_image)
+    CACHE_DIR = Path(Path.home(), 'Downloads', 'FlatIron', lab, 'Subjects', subject, 'histology')
+    CACHE_DIR.mkdir(exist_ok=True, parents=True)
+    path_to_image = Path(CACHE_DIR, tif_files[0])
+    if not path_to_image.exists():
+        url = (baseurl + '/' + tif_files[0])
+        http_download_file(url, cache_dir=CACHE_DIR,
+                           username=par.HTTP_DATA_SERVER_LOGIN,
+                           password=par.HTTP_DATA_SERVER_PWD)
 
-        return path_to_nrrd
+    path_to_nrrd = tif2nrrd(path_to_image)
 
+    return path_to_nrrd
+
+    #else:
+    #    tif_files = []
+    #    for line in r.text.splitlines():
+    #        result = re.findall('href="(.*)GR.tif"', line)
+    #        if result:
+    #            tif_files.append(result[0] + 'GR.tif')
+#
+    #    CACHE_DIR = Path(Path.home(), 'Downloads', 'FlatIron', lab, 'Subjects', subject, 'histology')
+    #    CACHE_DIR.mkdir(exist_ok=True, parents=True)
+    #    path_to_image = Path(CACHE_DIR, tif_files[0])
+    #    if not path_to_image.exists():
+    #        url = (baseurl + '/' + tif_files[0])
+    #        http_download_file(url, cache_dir=CACHE_DIR,
+    #                           username=par.HTTP_DATA_SERVER_LOGIN,
+    #                           password=par.HTTP_DATA_SERVER_PWD)
+#
+    #    path_to_nrrd = tif2nrrd(path_to_image)
+#
+    #    return path_to_nrrd
+#
 
 def tif2nrrd(path_to_image):
     path_to_nrrd = Path(path_to_image.parent, path_to_image.parts[-1][:-3] + 'nrrd')
