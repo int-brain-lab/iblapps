302c8c0ea3d2baa842d96e1fd0ea1ed7706284de
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-09-03 14:32:57 +0100

    link to __main__

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     entry_points={
         'console_scripts': [
             'atlas=atlasview.atlasview:main',
-            'align=atlaselectrophysiology.ephys_atlas_gui:main',
+            'align=atlaselectrophysiology.ephys_atlas_gui:__main__',
         ]
     },
 )
