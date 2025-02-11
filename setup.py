24ad293f2d2e681bfa7cf5a46a6aba75e16e3a95
Olivier Winter <olivier.winter@hotmail.fr>
2025-02-11 16:19:26 +0000

    bump minimum Python version to 3.10

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='iblapps',
     version='0.1',
+    python_requires='>=3.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=require,
